e20e51673bb9570a20c44d9b761557eba6ba8336
troy0915 <johntroydolom7@gmail.com>
2025-08-08 23:09:10 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity9even_or_odd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get input from the user
+            Console.Write("Enter a number: ");
+            string input = Console.ReadLine();
+
+            // Validate input
+            if (int.TryParse(input, out int number))
+            {
+                // Check if the number is even or odd
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine("The number is even.");
+                }
+                else
+                {
+                    Console.WriteLine("The number is odd.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error: Please enter a valid integer.");
+            }
+        }
+    }
+}
+
+
+
+
+
